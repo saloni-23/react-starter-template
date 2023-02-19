a23d1fd7e23d00c5eef723176b5aae580f5b257b
Saloni Seth <saloni.seth@ril.com>
2023-02-19 16:20:34 +0530

    Keep calm and commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  conosle.log("yest")
+  console.log("test thoroughly");
   return (
     <div className="App">
       <header className="App-header">
