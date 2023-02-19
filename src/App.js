78f6521c33df91036fd8c61b3fbe57102bdd29bb
Saloni Seth <saloni.seth@ril.com>
2023-02-19 18:11:15 +0530

    Keep calm and commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to test.
+          test here Edit <code>src/App.js</code> and save to test.
         </p>
         <a
           className="App-link"
